7317b6e4106a7971df6c29352eeab1ea090af0bc
clay <82051128+machine1337@users.noreply.github.com>
2022-11-18 12:31:40 -0800

    Update fakesms.py

--- fakesms.py
+++ fakesms.py
@@ -16,11 +16,7 @@ except ImportError:
     print("[*]Installing termcolor Module")
     os.system("pip install termcolor -q -q -q")
     import termcolor
-try:
-    from coluroma import colors
-except:
-    os.system("pip install coluroma -q -q -q")
-    from coluroma import colors
+
 def logo():
     print(termcolor.colored('''
                _______ _    _  _______     ____  __  __ ______
